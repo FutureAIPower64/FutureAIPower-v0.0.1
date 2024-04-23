bc417a5d9de88ec8df094cf67245fbbb99af0864
VanshRadadiya <vanshradadiya95@gmail.com>
2024-04-23 11:00:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import DummyData from './components/DummyData';
 import ToggleSwitch from './components/ToggleSwitch';
+import Header from './components/Header';
 
 function App() {
   let [mode,setMode] = useState(false);
@@ -8,6 +9,7 @@ function App() {
     <div data-mode={mode ? "dark" : "light"}>
       <ToggleSwitch mode={mode} setMode={setMode}/>
       <DummyData />
+      <Header></Header>
     </div>
   )
 }
