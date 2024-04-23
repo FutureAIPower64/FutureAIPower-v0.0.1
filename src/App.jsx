84372f4fd1c862f8c80f406999fd999c7fd3e3fa
heremohitkikani <mkikani215@gmail.com>
2024-04-23 11:02:36 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import DummyData from './components/DummyData';
 import ToggleSwitch from './components/ToggleSwitch';
-import Header from './Header';
+import Header from './components/Header';
 
 function App() {
   let [mode,setMode] = useState(false);
