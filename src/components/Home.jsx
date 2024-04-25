e4c42326ac62422fb4934b0c5346131345c2cbaf
VanshRadadiya <vanshradadiya95@gmail.com>
2024-04-25 11:53:06 +0530

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,16 @@ import ToggleSwitch from "./ToggleSwitch";
 import DummyData from "./DummyData";
 import Header from "./Header";
 import Box from "./Box";
+import Sliderbar from "./Sliderbar";
 function Home() {
   let [mode, setMode] = useState(false);
 
   return (
     <div data-mode={mode ? "dark" : "light"}>
       <Header />
-      <ToggleSwitch mode={mode} setMode={setMode} />
-      <DummyData />
+      {/* <ToggleSwitch mode={mode} setMode={setMode} /> */}
+      {/* <DummyData /> */}
+      <Sliderbar></Sliderbar>
       <Box></Box>
     </div>
   );
