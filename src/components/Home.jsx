6b223b8d7f41a5a782ad4dbf207f84e0a3c1afee
VanshRadadiya <vanshradadiya95@gmail.com>
2024-05-04 17:04:25 +0530

    vansh-wok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import Header from './Header'
 import Box from './Box'
 import Cards from './Cards'
+import Sliderbar from './Sliderbar'
 
 function Home() {
   return (
     <div >
       <Header />
-      {/* <Sliderbar /> */}
-      {/* <Cards></Cards> */}
       <Box />
+      <div className='mt-14'>
+        <div className='flex'>
+          <div className='w-2/12'>
+            <Sliderbar></Sliderbar>
+          </div>
+          <div className='w-10/12'>
+            <Cards></Cards>
+            <Cards></Cards>
+            <Cards></Cards>
+            <Cards></Cards>
+            <Cards></Cards>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
