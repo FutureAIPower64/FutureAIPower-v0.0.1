1a6ea71137c4aa47f0178a15598389b617cfe40f
VanshRadadiya <vanshradadiya95@gmail.com>
2024-05-28 11:15:56 +0530

    backend

---
+++ back-end/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var user = require('../controller/usercontroller');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
+/* GET home page. */
+router.post('/',upload.single('images'), user.insert);
+
+module.exports = router;
